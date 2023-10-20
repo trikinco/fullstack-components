fc384180bd8ef983acf46fbf5b9e75e10d813dc3
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-20 14:16:23 +1100

    chore: moves tailwind base CSS file to src/styles
    - removes invalid @storybook/themes that was automatically added by @storybook/addon-themes configuration script

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,13 +3,12 @@ import type { StorybookConfig } from '@storybook/nextjs'
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-onboarding',
-        '@storybook/addon-interactions',
-        '@storybook/addon-themes',
-        '@storybook/themes'
-    ],
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-onboarding',
+		'@storybook/addon-interactions',
+		'@storybook/addon-themes',
+	],
 	framework: {
 		name: '@storybook/nextjs',
 		options: {},
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import '../src/app/globals.css'
+import '../src/styles/globals.css'
 import type { Preview } from '@storybook/react'
 
 import { withThemeByDataAttribute } from '@storybook/addon-themes'
