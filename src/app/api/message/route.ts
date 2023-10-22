4e64b6829ab755e2f9bb14e1f9edc15b20ea3a3f
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-22 19:00:34 +1100

    feat: simple poc for generating user friendly error messages
    - adds openapi dep
    - adds buttons to /app/page for generating different errors
    - adds /api/error for generating errors
    - adds /api/message for handling error prompts

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,92 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+})
+
+/**
+ * Get a prompt for generating a user-friendly error message
+ *
+ * @see https://wix-ux.com/when-life-gives-you-lemons-write-better-error-messages-46c5223e1a2f
+ * @see https://www.linkedin.com/pulse/designing-better-error-messages-ux-vitaly-friedman/
+ * @see https://www.nngroup.com/articles/error-message-guidelines/
+ */
+function generateErrorMessagePrompt(message: string, stack: string) {
+	return `Suggest a concise and user-friendly error message based on the following details.
+
+    Error message: ${message}
+    Error stack trace: ${stack}
+
+    Format the user-friendly error message based on these best practices:
+    - Title: Say what happened.
+    - Body (not bullet points): If possible provide reassurance by letting the user know what was not affected by the error, say why the error happened, help them fix it, give them a way out
+	
+	Simplify the error message to be readable at an 8th-grade level.
+
+	Avoid: Whoops!, Oops!, Resource loading error, Something went wrong, try again later, the requested resource, sorry for the inconvenience, technical jargon, generic response
+    `
+}
+
+export async function POST(req: NextRequest) {
+	let error = 'Internal Server Error'
+
+	if (!openai.apiKey) {
+		error =
+			'OpenAI API key not configured, please follow the instructions in README.md'
+
+		return NextResponse.json(
+			{
+				error,
+			},
+			{
+				status: 500,
+				statusText: error,
+			}
+		)
+	}
+
+	const { message, stack } = await req.json()
+
+	if (!message || !stack) {
+		error = 'Please provide an error message'
+
+		return NextResponse.json(
+			{
+				error,
+			},
+			{
+				status: 400,
+				statusText: error,
+			}
+		)
+	}
+
+	try {
+		const chatCompletion = await openai.chat.completions.create({
+			messages: [
+				{ role: 'user', content: generateErrorMessagePrompt(message, stack) },
+			],
+			model: 'gpt-3.5-turbo',
+		})
+
+		return NextResponse.json(
+			{
+				result: chatCompletion.choices,
+			},
+			{
+				status: 200,
+			}
+		)
+	} catch (e) {
+		return NextResponse.json(
+			{
+				error,
+			},
+			{
+				status: 500,
+				statusText: error,
+			}
+		)
+	}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,72 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+	const [result, setResult] = useState()
+
+	/**
+	 * Placeholder for generating some generic errors
+	 */
+	async function handleError(status = 500) {
+		try {
+			const response = await fetch('/api/error', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ status }),
+			})
+
+			if (!response.ok) {
+				throw new Error(
+					`Failed to load resource: the server responded with a status of ${response.status} (${response.statusText})`
+				)
+			}
+		} catch (error) {
+			return await fetchErrorMessage(error as Error)
+		}
+	}
+
+	/**
+	 * Placeholder for passing an error prompt to OpenAI and generating a user-friendly message
+	 */
+	async function fetchErrorMessage(error: Error) {
+		try {
+			const response = await fetch('/api/message', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ message: error.message, stack: error.stack }),
+			})
+
+			const data = await response.json()
+
+			if (response.status !== 200) {
+				throw (
+					data.error ||
+					new Error(`Request failed with status ${response.status}`)
+				)
+			}
+			console.log(data.result)
+			setResult(data.result)
+		} catch (error) {
+			/**
+			 * Continue handling other/fallback cases here
+			 * @consideration flag for turning on navigator.onLine check when applicable for the application
+			 */
+			if (error instanceof Error && error.message) {
+				console.error({
+					message: error.message,
+					stack: error.stack,
+				})
+			} else {
+				console.error(error)
+			}
+		}
+	}
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -28,85 +94,30 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-				<Image
-					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
-			</div>
-
 			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+				<button
+					onClick={() => handleError()}
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Docs{' '}
+						Generate 500 error{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about Next.js features and API.
-					</p>
-				</a>
+				</button>
 
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+				<button
+					onClick={() => handleError(400)}
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Learn{' '}
+						Generate 400 error{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Templates{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Explore the Next.js 13 playground.
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Deploy{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Instantly deploy your Next.js site to a shareable URL with Vercel.
-					</p>
-				</a>
+				</button>
 			</div>
 		</main>
 	)
