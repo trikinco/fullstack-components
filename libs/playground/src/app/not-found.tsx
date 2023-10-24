c4a31ab203b86b9d8fca7f75ea99c2f462689299
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-24 12:50:06 +1100

    feat: adds a root not-found to only use @error

---
+++ libs/playground/src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google'
+import '../styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+/**
+ * Uses parallel routing to only render @error/error/page,
+ * and not the children
+ *
+ * @note that this disables the default next js 404 page
+ */
+export default function RootNotFound({
+	error,
+}: {
+	// Do not render children - only @error/error instead - see `app/layout`
+	children: React.ReactNode
+	/**
+	 * Error page slot - see `app/@error`
+	 * @see {@link https://nextjs.org/docs/app/building-your-application/routing/parallel-routes Parallel routes}
+	 */
+	error: React.ReactNode
+}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{error}</body>
+		</html>
+	)
+}
