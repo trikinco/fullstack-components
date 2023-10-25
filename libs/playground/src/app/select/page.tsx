613647da1cb30fcf7d02ee7e63bafbc430b695d1
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-25 21:11:11 +1100

    chore: update demo with a different example

--- libs/playground/src/app/select/page.tsx
+++ libs/playground/src/app/select/page.tsx
@@ -10,13 +10,16 @@ export default function Page() {
 			<div className="bg-white/10 mb-4 block p-4 rounded-md border-2 border-white/50">
 				<code className="mb-3 block">Code</code>
 				<code className="prose dark:prose-invert">
-					&lt;Select purpose=&quot;selecting a timezone&quot;
-					context=&quot;I&apos;m in Brisbane&quot; /&gt;
+					&lt;Select purpose=&quot;selecting a timezone&quot; context=&quot;our
+					users are in Sydney&quot; /&gt;
 				</code>
 			</div>
 
 			<div className="max-w-prose md:min-w-[65ch]">
-				<Select purpose="selecting a timezone" context="I'm in Brisbane" />
+				<Select
+					purpose="selecting a timezone"
+					context="our users are in Sydney"
+				/>
 			</div>
 		</main>
 	)
