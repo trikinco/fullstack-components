1fe10d59bfd7b9bfbd983982a5794ac1125b0686
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-30 18:44:50 +1100

    feat: make Button polymorphic

--- libs/playground/src/components/Button.tsx
+++ libs/playground/src/components/Button.tsx
@@ -1,20 +1,25 @@
-import type { HTMLAttributes, ReactNode } from 'react'
+import type { ElementType, HTMLAttributes } from 'react'
+import type { AsComponent } from '../types/AsComponent'
 import { merge } from '../utils/styles'
 
-export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-	/**
-	 * Button contents
-	 */
-	children?: ReactNode
-}
+export type ButtonProps = HTMLAttributes<HTMLButtonElement>
+export const defaultElement = 'button'
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ className, children, ...props }: ButtonProps) => {
+export const Button = <C extends ElementType = typeof defaultElement>({
+	as,
+	className,
+	children,
+	...props
+}: AsComponent<C, ButtonProps>) => {
+	const Component = as || defaultElement
+	const type = !as || as === defaultElement ? 'button' : undefined
+
 	return (
-		<button
-			type="button"
+		<Component
+			type={type}
 			className={merge(
 				'bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400',
 				className
@@ -22,6 +27,6 @@ export const Button = ({ className, children, ...props }: ButtonProps) => {
 			{...props}
 		>
 			{children}
-		</button>
+		</Component>
 	)
 }
