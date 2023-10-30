c0f401515e331160d73aa1254042ace4e510d4fb
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-30 18:44:21 +1100

    chore: adds name and github url to constants

--- libs/playground/src/utils/constants.ts
+++ libs/playground/src/utils/constants.ts
@@ -1,3 +1,7 @@
+export const NAME_LONG = 'AI-Powered Fullstack Components'
+export const NAME_SHORT = 'Fullstack Components'
+export const URL_GITHUB = 'https://github.com/ambient-co/fullstack-components'
+
 export const DEPLOYMENT_URL =
 	process.env.VERCEL_ENV === 'preview'
 		? `https://${process.env.VERCEL_URL}`
