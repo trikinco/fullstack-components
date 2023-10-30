f96c4eba4f8a9203519f0181fd140f9ac4d2f1d2
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-10-30 18:42:51 +1100

    fix: adds image remote patterns to config

--- libs/playground/next.config.js
+++ libs/playground/next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-// just used for testing useEffect works as expected
-   // reactStrictMode: false
+	// just used for testing useEffect works as expected
+	// reactStrictMode: false
+	/**
+	 * Configure remote image patterns
+	 * for the Image demo
+	 */
+	images: {
+		remotePatterns: [
+			// Image description
+			{
+				protocol: 'https',
+				hostname: 'upload.wikimedia.org',
+			},
+			// Image creation
+			{
+				protocol: 'https',
+				hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
