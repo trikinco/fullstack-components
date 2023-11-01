ef76e958e1f73e59e0848aca79502857f26fe8e0
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-01 13:33:20 +1100

    fix: workaround for fetch failing undici

--- libs/playground/src/app/(demos)/layout.tsx
+++ libs/playground/src/app/(demos)/layout.tsx
@@ -1,5 +1,14 @@
 import type { ReactNode } from 'react'
 
+/**
+ * Temporary workaround for `fetch failed` `undici`
+ * @see https://github.com/vercel/next.js/issues/44062
+ *
+ * Force dynamic rendering and uncached data fetching of a layout or page
+ * by disabling all caching of fetch requests and always revalidating.
+ */
+export const dynamic = 'force-dynamic'
+
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<main className="flex flex-col items-center justify-between p-6 md:p-24 min-h-screen">
