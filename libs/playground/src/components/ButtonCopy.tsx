972da25da031e4cd9f1de676046ec5be0ded11f4
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-02 11:10:38 +1100

    feat: md custom code blocks with copy button

---
+++ libs/playground/src/components/ButtonCopy.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState, type HTMLAttributes } from 'react'
+import { IconCheck } from './Icons/IconCheck'
+import { IconFile } from './Icons/IconFile'
+import { merge } from '../utils/styles'
+
+export interface ButtonCopyProps extends HTMLAttributes<HTMLButtonElement> {
+	text?: string
+}
+
+/**
+ * Button used to copy `text` to the clipboard
+ */
+export const ButtonCopy = ({ className, text, ...rest }: ButtonCopyProps) => {
+	const [isCopied, setIsCopied] = useState(false)
+
+	const copy = async () => {
+		await navigator.clipboard.writeText(text?.trim() || '')
+		setIsCopied(true)
+
+		setTimeout(() => {
+			setIsCopied(false)
+		}, 3000)
+	}
+
+	return (
+		<button
+			className={merge(
+				'w-9 h-9 inline-flex items-center justify-center rounded-md border-2 border-slate-300 dark:border-white/20 hover:bg-slate-100 dark:hover:bg-white/10',
+				className
+			)}
+			disabled={isCopied}
+			onClick={copy}
+			{...rest}
+		>
+			{isCopied ? <IconCheck /> : <IconFile />}
+			<span className="sr-only">{isCopied ? 'Copied!' : 'Copy'}</span>
+		</button>
+	)
+}
