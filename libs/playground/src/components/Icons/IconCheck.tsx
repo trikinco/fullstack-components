9332c2a518639da90f8ed13bd2857b6fad1272d7
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-02 11:09:24 +1100

    chore: adds check and file icons, consistent icons

---
+++ libs/playground/src/components/Icons/IconCheck.tsx
@@ -0,0 +1,25 @@
+import type { HTMLAttributes } from 'react'
+import { merge } from '@/src/utils/styles'
+
+// https://www.svgrepo.com/collection/dazzle-line-icons/
+export const IconCheck = ({
+	className,
+	...rest
+}: HTMLAttributes<SVGSVGElement>) => (
+	<svg
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		className={merge('w-5 h-5', className)}
+		fill="none"
+		aria-hidden="true"
+		{...rest}
+	>
+		<path
+			d="M4 12.6111L8.92308 17.5L20 6.5"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+)
