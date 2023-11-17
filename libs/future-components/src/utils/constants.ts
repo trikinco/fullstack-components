2429286783d28bd4d14e4d6de8b6c7783925603b
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-17 15:44:21 +1100

    Revert "chore: URL_HOST to include VERCEL_URL in preview"
    This reverts commit 677f87ea25ca0131c9d0b6c484efd047e667b96f.

--- libs/future-components/src/utils/constants.ts
+++ libs/future-components/src/utils/constants.ts
@@ -1,5 +1,2 @@
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || 'sk-1234'
-export const URL_HOST =
-	process.env.VERCEL_ENV === 'preview'
-		? `https://${process.env.VERCEL_URL}`
-		: process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000'
+export const URL_HOST = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000'
