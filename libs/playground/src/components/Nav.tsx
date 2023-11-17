4dce49c1a364f13794b0feaf977daa1338fdb9dd
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-17 15:17:14 +1100

    feat: adds built in public topnav callout

--- libs/playground/src/components/Nav.tsx
+++ libs/playground/src/components/Nav.tsx
@@ -1,8 +1,7 @@
 import type { HTMLAttributes, ReactNode } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { merge } from '../utils/styles'
-import { NAME_SHORT, URL_GITHUB } from '../utils/constants'
+import { NAME_LIB, NAME_SHORT, URL_GITHUB } from '../utils/constants'
 import { IconGitHub } from './Icons/IconGitHub'
 import { IconLogo } from './Icons/IconLogo'
 
@@ -12,28 +11,50 @@ export interface NavProps extends HTMLAttributes<HTMLDivElement> {
 
 export const Nav = ({ children, className, ...rest }: NavProps) => {
 	return (
-		<nav
-			className={merge(
-				'flex sticky top-0 left-0 backdrop-blur-md items-center p-6',
-				className
-			)}
-			{...rest}
-		>
-			<Link
-				href="/"
-				className="rounded-full ring-offset-2 focus:outline-none focus:ring"
+		<>
+			<div className="flex justify-between text-sm p-6 bg-white dark:bg-transparent border-b border-slate-950/10 dark:border-white/30">
+				<p>
+					<strong>
+						<span role="img" aria-label="Hey there, hand waving">
+							👋
+						</span>{' '}
+						<Link href={URL_GITHUB}>{NAME_LIB}</Link> is built in public, and
+						not ready for production use.
+					</strong>{' '}
+				</p>
+				<p>
+					We&apos;re just getting started and{' '}
+					<Link
+						className="underline"
+						href="https://github.com/trikinco/fullstack-components/discussions"
+					>
+						feedback is encouraged.
+					</Link>
+				</p>
+			</div>
+			<nav
+				className={merge(
+					'flex sticky top-0 left-0 backdrop-blur-md items-center p-6',
+					className
+				)}
+				{...rest}
 			>
-				<IconLogo className="w-10 h-10" />
-			</Link>
-			{children}
+				<Link
+					href="/"
+					className="rounded-full ring-offset-2 focus:outline-none focus:ring"
+				>
+					<IconLogo className="w-10 h-10" />
+				</Link>
+				{children}
 
-			<Link
-				href={URL_GITHUB}
-				className="ml-auto block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 rounded-full ring-offset-2 focus:outline-none focus:ring"
-			>
-				<span className="sr-only">{NAME_SHORT} on GitHub</span>
-				<IconGitHub />
-			</Link>
-		</nav>
+				<Link
+					href={URL_GITHUB}
+					className="ml-auto block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 rounded-full ring-offset-2 focus:outline-none focus:ring"
+				>
+					<span className="sr-only">{NAME_SHORT} on GitHub</span>
+					<IconGitHub />
+				</Link>
+			</nav>
+		</>
 	)
 }
