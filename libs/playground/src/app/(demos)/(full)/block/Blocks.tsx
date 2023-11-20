09ac2a9136b99d91aabbc8fcd04c486955d59205
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-20 11:46:23 +1100

    feat: Blocks demo page
    - adds reusable Prose component

---
+++ libs/playground/src/app/(demos)/(full)/block/Blocks.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import Script from 'next/script'
+import { Block } from '@trikinco/fullstack-components/client'
+import { Spinner } from '@/src/components/Spinner'
+import { NAME_LIB, NAME_SHORT, NAME_LONG } from '@/src/utils/constants'
+
+export default function Blocks() {
+	return (
+		<>
+			{/* Temporary solution -> trigger build during esm step in Block */}
+			<Script src="https://cdn.tailwindcss.com" />
+
+			<Block
+				prompt={`A navbar with a modern SVG logo, ${NAME_LIB} and some links. Dark bg, light text.`}
+				fallback={
+					<Spinner
+						className="flex gap-3 p-3 items-center"
+						classNameSpinner="mb-0"
+					>
+						Creating navbar...
+					</Spinner>
+				}
+			/>
+			<Block
+				prompt={`A sleek UI hero section with some text about ${NAME_LONG}. Dark bg, light text.`}
+				fallback={
+					<Spinner
+						className="flex gap-3 p-3 items-center"
+						classNameSpinner="mb-0"
+					>
+						Creating hero section...
+					</Spinner>
+				}
+			/>
+			<Block
+				prompt={`A two-column call to action section with a purple button and some text listing some features of ${NAME_LONG} in one column, and a photo from Unsplash in the other column. Dark bg, light text.`}
+				fallback={
+					<Spinner
+						className="flex gap-3 p-3 items-center"
+						classNameSpinner="mb-0"
+					>
+						Creating call to action section...
+					</Spinner>
+				}
+			/>
+			<Block
+				prompt={`A footer with copyright for this year - ${NAME_SHORT}. Dark bg, light text.`}
+				fallback={
+					<Spinner
+						className="flex gap-3 p-3 items-center"
+						classNameSpinner="mb-0"
+					>
+						Creating footer...
+					</Spinner>
+				}
+			/>
+		</>
+	)
+}
--- libs/playground/src/app/docs/layout.tsx
+++ libs/playground/src/app/docs/layout.tsx
@@ -1,9 +1,10 @@
+import Prose from '@/src/components/Prose'
 import type { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<main className="flex flex-col items-center justify-between p-6 md:p-24 min-h-screen">
-			<div className="prose w-full dark:prose-invert mx-auto">{children}</div>
+			<Prose>{children}</Prose>
 		</main>
 	)
 }
