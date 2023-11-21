3c3ed835c2fb56b9191e608eae5dcf8c20464d8b
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-21 16:06:11 +1100

    chore: update siteUrl in sitemap config

--- libs/playground/next-sitemap.config.js
+++ libs/playground/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000/',
+	siteUrl: process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000/',
 	generateRobotsTxt: true,
 	generateIndexSitemap: false,
 }
