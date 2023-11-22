0d93f353b263ffbda4cdde4f4fde8d725dbdb015
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-22 11:51:14 +1100

    refactor: allow useRequest to accept a fetcher

--- libs/future-components/src/hooks/useRequest.ts
+++ libs/future-components/src/hooks/useRequest.ts
@@ -4,16 +4,45 @@ import { request } from '../utils/request'
 import type { RequestConfig } from '../types'
 
 /** Fetch request config initialiser in addition to the `baseUrl` */
-export type UseRequestConfig<Tbody = unknown> = RequestConfig<Tbody> & {
+export type UseRequestConfig<
+	TResponse = unknown,
+	Tbody = unknown,
+> = RequestConfig<Tbody> & {
 	/** Whether the hook should run or not */
 	isEnabled?: boolean
+	/** Custom fetcher function enabling replacement of the built-in `request` in `requestData`  */
+	fetcher?: (
+		/** Relative API url */
+		url: string
+	) => Promise<TResponse>
+}
+
+/** Config for hooks consuming `useRequest` */
+export type UseRequestConsumerConfig<Tbody = unknown> = Omit<
+	UseRequestConfig<unknown, Tbody>,
+	'fetcher'
+>
+
+const requestData = <TResponse = unknown, Tbody = unknown>(
+	/** Relative API url */
+	url: string,
+	/** Fetch request config initialiser in addition to the `baseUrl` */
+	config: UseRequestConfig<TResponse, Tbody>
+) => {
+	const { fetcher, ...requestConfig } = config
+
+	if (fetcher !== undefined) {
+		return fetcher(url)
+	}
+
+	return request<TResponse, Tbody>(url, requestConfig)
 }
 
 export const useRequest = <TResponse = unknown, Tbody = unknown>(
 	/** Relative API url */
 	url: string,
 	/** Fetch request config initialiser in addition to the `baseUrl` */
-	config: UseRequestConfig<Tbody>
+	config: UseRequestConfig<TResponse, Tbody>
 ) => {
 	const { isEnabled = true, ...requestConfig } = config
 	const isEnabledReference = useRef(isEnabled)
@@ -28,7 +57,7 @@ export const useRequest = <TResponse = unknown, Tbody = unknown>(
 		setIsLoading(true)
 
 		try {
-			const json = await request<TResponse, Tbody>(url, requestConfig)
+			const json = await requestData<TResponse, Tbody>(url, requestConfig)
 
 			setData(json)
 			setError(null) // eslint-disable-line unicorn/no-null
