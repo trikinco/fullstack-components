01bbacfebd558c79245696f81f9699f784f8b28f
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-22 11:48:33 +1100

    chore: removes unused backgroundImage tw config

--- libs/playground/tailwind.config.ts
+++ libs/playground/tailwind.config.ts
@@ -9,11 +9,6 @@ const config: Config = {
 			mono: ['var(--font-mono)', ...defaultTheme.fontFamily.mono],
 		},
 		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
 			animation: {
 				typewriter: 'typing 2s steps(30, end)',
 				writing: 'caret 1.5s step-end infinite',
