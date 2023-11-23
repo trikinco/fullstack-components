1f937a1ced54e98cd397c0d3b13d37f1f5209c9c
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-23 16:23:06 +1100

    chore: updated text prompt for consistency

--- libs/future-components/src/handlers/text/textClient.ts
+++ libs/future-components/src/handlers/text/textClient.ts
@@ -2,14 +2,15 @@ import { runChatCompletion } from '../../chatGptService'
 import { OPENAI_API_KEY } from '../../utils/constants'
 import type { TextRequestBody, TextOptions } from './models'
 
-const systemPrompt = `Rewrite and modify text content for the web provided by the user. 
+const systemPrompt = `Rewrite, create, edit and modify text content for the web provided by the user.
+You have full creative licence, unless otherwise specified by the user.
 You accept HTML, markdown and plain text. 
-The user will provide you with 'Rules' for how to modify the text.
+The user will provide you with 'Rules' for how to modify the text. Only act on the content after 'Text:', DO NOT include the prompt or 'Rules'.
 
 ## Instructions
-You ONLY RETURN JSON WITH THE FOLLOWING STRUCTURE {content:Array<{type:'text'|'markdown'|'HTML',value:string}>}
-Each item in the 'content' array is a separate paragraph/title/text
-If you receive plain text, markdown or HTML, return the stringified content in the JSON 'content' array 'value' with the appropriate 'type': 'text' | 'markdown' | 'HTML'
+You ONLY RETURN JSON WITH THE FOLLOWING STRUCTURE {type:'text'|'markdown'|'HTML',content:string}
+Return a single appropriate 'type' of 'text' | 'markdown' | 'HTML', either matching the input content type, or a 'type' defined by the user. This type MUST MATCH the actual kind of content you return. 
+Return all the stringified content in the JSON 'content' property
 If the user's HTML text has HTML heading elements (h1, h2, h3 etc.) and your rewritten text includes headings, ensure that they START AT THE SAME LEVEL as the input. e.g user prompt: 'make this text shorter', user HTML: '<h3 class="font-bold">long title</h3><p>long text..</p><p>more text</p>', return: '<h3 class="font-bold">title</h3><p>text..</p>'.
 `
 
@@ -18,8 +19,9 @@ export class TextClient {
 		console.log('handling text request', request)
 
 		const {
-			prompt = 'Rewrite this text',
+			prompt = 'Rewrite the text',
 			content,
+			type,
 			tone,
 			strength,
 			grade,
@@ -28,20 +30,23 @@ export class TextClient {
 		} = request
 
 		const rules = [
-			tone ? `Your tone of voice when rewriting the text is "${tone}".` : '',
+			type
+				? `All items returned in the 'content' array must be of 'type': ${type}`
+				: '',
+			tone
+				? `The style or tone of voice when rewriting the text is "${tone}".`
+				: '',
 			strength
-				? `You rewrite the text to "${strength}" on a scale from 0-100. 0 returns the original text, 100 completely rewrites the text.`
+				? `Your creative freedom strength is "${strength}" on a scale from 0-100. 0 is no creative freedom (return the original text, only correcting grammar), 100 is full creative freedom and imagination.`
 				: '',
-			grade
-				? `Aim to make the text suitable for a ${grade} reading level.`
-				: '',
+			grade ? `Make the text readable at a ${grade} reading level.` : '',
 			max && !min ? 'Make the full text shorter!' : '',
 			max
-				? `Limit the full text to ${max} characters or less! RETURN LESS THAN ${max} CHARACTERS TOTAL.`
+				? `Limit the full text to ${max} characters or less! YOU MUST RETURN LESS THAN ${max} CHARACTERS. THIS RULE HAS PRIORITY.`
 				: '',
 			min && !max ? 'Make the full text longer!' : '',
 			min
-				? `Increase the full text to ${min} characters or more! RETURN MORE THAN ${min} CHARACTERS TOTAL.`
+				? `Increase the full text to ${min} characters or more! YOU MUST RETURN MORE THAN ${min} CHARACTERS. THIS RULE HAS PRIORITY.`
 				: '',
 		]
 			.filter(Boolean)
@@ -52,7 +57,12 @@ export class TextClient {
 				{ role: 'system', content: systemPrompt },
 				{
 					role: 'user',
-					content: `# ${prompt}\nText: ${content}\n## Rules\n${rules}`,
+					content: `# ${prompt}
+					Rules: 
+					${rules}
+					Text: 
+					${content}
+					`,
 				},
 			],
 			{
