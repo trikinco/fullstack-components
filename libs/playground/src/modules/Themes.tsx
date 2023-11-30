b76c7eb6ce58e2fcaee03facc3d4f5f1ec2dffbf
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-11-30 22:03:27 +1100

    chore: Safari theme toggler styles

--- libs/playground/src/modules/Themes.tsx
+++ libs/playground/src/modules/Themes.tsx
@@ -38,33 +38,35 @@ export const Themes = memo(function Themes({
 	setTheme,
 }: ThemesProps) {
 	return (
-		<fieldset id={id} className="flex gap-3">
+		<fieldset id={id} className="flex flex-col gap-3 text-right max-h-fit">
 			<legend className="inline-flex gap-2 tracking-wide text-sm mb-8 px-1 ml-auto">
 				Theme
 				<ThemeIcon theme={theme} />
 			</legend>
-			{themes.map(({ name, image }) => (
-				<label
-					key={name}
-					className="group flex flex-col gap-2 -mt-2 cursor-pointer"
-				>
-					<span className="sr-only">Change theme to {name}</span>
-					<input
-						type="radio"
-						name="theme"
-						value={name}
-						defaultChecked={theme === name}
-						className="appearance-none peer"
-						onChange={() => setTheme(name)}
-					/>
-					<span className="group/icon bg-white dark:bg-black w-20 h-20 rounded-md ring-black ring-offset-2 dark:ring-white dark:ring-offset-black peer-focus-visible:ring-blue-400 peer-focus-visible:ring-offset-inherit opacity-80 hover:opacity-100 peer-checked:opacity-100 peer-checked:ring-2 transition-opacity ease-in-out">
-						<span className="flex transform overflow-hidden rounded-md bg-gradient-to-tr from-[#B5FFFC] to-[#FFDEE9]">
-							{image}
+			<div className="flex gap-3 text-left">
+				{themes.map(({ name, image }) => (
+					<label
+						key={name}
+						className="group flex flex-col gap-2 -mt-2 cursor-pointer"
+					>
+						<span className="sr-only">Change theme to {name}</span>
+						<input
+							type="radio"
+							name="theme"
+							value={name}
+							defaultChecked={theme === name}
+							className="appearance-none peer"
+							onChange={() => setTheme(name)}
+						/>
+						<span className="group/icon bg-white dark:bg-black w-20 h-20 rounded-md ring-black ring-offset-2 dark:ring-white dark:ring-offset-black peer-focus-visible:ring-blue-400 peer-focus-visible:ring-offset-inherit opacity-80 hover:opacity-100 peer-checked:opacity-100 peer-checked:ring-2 transition-opacity ease-in-out">
+							<span className="flex transform overflow-hidden rounded-md bg-gradient-to-tr from-[#B5FFFC] to-[#FFDEE9]">
+								{image}
+							</span>
 						</span>
-					</span>
-					<span className="capitalize mx-2">{name}</span>
-				</label>
-			))}
+						<span className="capitalize mx-2">{name}</span>
+					</label>
+				))}
+			</div>
 		</fieldset>
 	)
 })
