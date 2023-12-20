48bf136cb342591f86919261ab93ce0b650938a0
Lars Magnus Klavenes <larsmagnus@gmail.com>
2023-12-20 20:53:19 +0100

    fix(prompt): adds typed responses to usePrompt
    - adds type variable to usePrompt to allow for generic types

--- packages/fullstack-components/src/handlers/prompt/usePrompt.ts
+++ packages/fullstack-components/src/handlers/prompt/usePrompt.ts
@@ -11,18 +11,30 @@ import { ApiUrlEnum } from '../../enums/ApiUrlEnum'
  * A client-side fetch handler hook that answers a `prompt` or an array of `messages` and returns the response as-is.
  * @see `ApiUrlEnum.prompt`
  */
-export function usePrompt(
+export function usePrompt<T = PromptResponseBody>(
 	/**
 	 * @link PromptRequestBody
 	 */
-	body: PromptRequestBody,
+	body: PromptRequestBody & { format?: 'text' | null },
 	/**
 	 * Fetch utility hook request options without the `fetcher`. Allows for overriding the default `request` config.
 	 * @link https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
 	 */
 	config?: UseRequestConsumerConfig<PromptRequestBody>
+): ReturnType<typeof useRequest<T, PromptRequestBody>>
+export function usePrompt<
+	T = {
+		[k: string]: unknown
+	},
+>(
+	body: PromptRequestBody & { format: 'JSON' },
+	config?: UseRequestConsumerConfig<PromptRequestBody>
+): ReturnType<typeof useRequest<T, PromptRequestBody>>
+export function usePrompt<T = PromptResponseBody>(
+	body: PromptRequestBody,
+	config?: UseRequestConsumerConfig<PromptRequestBody>
 ) {
-	return useRequest<PromptResponseBody>(ApiUrlEnum.prompt, {
+	return useRequest<T>(ApiUrlEnum.prompt, {
 		body,
 		...config,
 	})
