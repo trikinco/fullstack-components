7ff3afc0fae305c99303f5bb213d28e2d14fb55a
Lars Magnus Klavenes <larsmagnus@gmail.com>
2024-01-11 15:29:22 +1100

    chore(site): updates search to allow for solo mode

--- packages/site/src/modules/Search/Search.tsx
+++ packages/site/src/modules/Search/Search.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { merge } from '@trikinco/fullstack-components/utils'
+import { Label } from '@/src/components/Elements/Label'
 import { SearchInput } from '@/src/modules/Search/SearchInput'
 import { SearchOptions } from '@/src/modules/Search/SearchOptions'
 import { useSearch, type UseSearchProps } from './useSearch'
@@ -7,9 +8,21 @@ import { useSearch, type UseSearchProps } from './useSearch'
 export interface SearchProps extends UseSearchProps {
 	className?: string
 	/**
+	 * If used full width on a page
+	 */
+	isSolo?: boolean
+	/**
+	 * Search input label when used `isSolo`
+	 */
+	label?: string
+	/**
 	 * The search input placeholder
 	 */
 	placeholder?: string
+	/**
+	 * Hides the search hotkey
+	 */
+	hideKey?: boolean
 	/**
 	 * Search input on active
 	 */
@@ -23,7 +36,10 @@ export function Search({
 	onChange,
 	onActive,
 	className,
+	label,
+	isSolo,
 	placeholder,
+	hideKey,
 	value,
 	loading,
 	error,
@@ -56,10 +72,11 @@ export function Search({
 				/>
 			)}
 
-			<div className={merge('relative z-20 md:w-64', className)}>
+			<div className={merge('relative z-20 w-full', className)}>
+				{label && <Label htmlFor={`${id}-search`}>{label}</Label>}
 				<SearchInput
 					ref={inputRef}
-					id={id}
+					id={`${id}-search`}
 					value={value}
 					placeholder={placeholder}
 					isFocused={isFocused}
@@ -72,6 +89,12 @@ export function Search({
 					onCompositionStart={handleComposition}
 					onCompositionEnd={handleComposition}
 					onKeyDown={handleKeydown}
+					hideKey={hideKey}
+					maxLength={100}
+					aria-controls={id}
+					aria-expanded={isFocused}
+					aria-activedescendant={`${id}-option-${activeResult}`}
+					role="combobox"
 				/>
 
 				<ul
@@ -81,12 +104,16 @@ export function Search({
 					aria-label="Search results"
 					className={merge(
 						'border border-gray-200 bg-white text-gray-100 dark:border-slate-700 dark:bg-slate-800',
-						'fixed md:absolute top-full mt-2 overflow-auto overscroll-contain rounded-xl py-2.5 shadow-xl',
-						'max-h-[min(calc(50vh-11rem-env(safe-area-inset-bottom)),400px)]',
+						'absolute top-full mt-2 overflow-auto overscroll-contain rounded-xl py-2.5 shadow-xl',
+						'w-screen min-h-[100px]',
+						'max-h-[min(calc(50vh-7rem-env(safe-area-inset-bottom)),400px)]',
+						isSolo
+							? 'max-w-[min(calc(100vw-3rem),100%)]'
+							: // Wider when used in a navbar
+								'max-w-[min(calc(100vw-3rem),calc(100%+20rem))]',
 						'md:max-h-[min(calc(100vh-5rem-env(safe-area-inset-bottom)),400px)]',
 						'inset-x-0 md:left-auto mx-auto',
 						'contrast-more:border contrast-more:border-gray-900 contrast-more:dark:border-gray-50',
-						'w-screen min-h-[100px] max-w-[min(calc(100vw-2rem),calc(100%+20rem))]',
 						shouldShowList ? 'flex flex-col' : 'hidden'
 					)}
 				>
