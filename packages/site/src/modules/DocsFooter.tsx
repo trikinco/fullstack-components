daaceb6e1483e13c3f39c8cc68b225e7c3a843ab
Lars Magnus Klavenes <larsmagnus@gmail.com>
2024-01-11 19:52:15 +1100

    chore(site): adds edit this page on GitHub link

--- packages/site/src/modules/DocsFooter.tsx
+++ packages/site/src/modules/DocsFooter.tsx
@@ -4,7 +4,9 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { merge } from '@trikinco/fullstack-components/utils'
 import { routesDocsMeta } from '@/src/utils/routes'
-import { IconChevronLeft } from '../components/Icons/IconChevronLeft'
+import { IconChevronLeft } from '@/src/components/Icons/IconChevronLeft'
+import { URL_GITHUB_EDIT } from '@/src/utils/constants'
+import { IconGitHub } from '@/src/components/Icons/IconGitHub'
 
 export interface DocsFooterProps extends HTMLAttributes<HTMLDivElement> {
 	children?: ReactNode
@@ -25,7 +27,7 @@ export default function DocsFooter({ children, className }: DocsFooterProps) {
 	return (
 		<footer
 			className={merge(
-				'flex gap-6 justify-between py-6 print:hidden',
+				'flex flex-col gap-8 justify-between py-6 print:hidden',
 				className
 			)}
 		>
@@ -44,6 +46,17 @@ export default function DocsFooter({ children, className }: DocsFooterProps) {
 						<IconChevronLeft width={20} height={20} className="rotate-180" />
 					</Link>
 				)}
+			</div>
+			<div className="w-full max-w-prose mx-auto flex items-center flex-wrap pt-8 border-t border-slate-200 dark:border-slate-800">
+				<Link
+					href={`${URL_GITHUB_EDIT}${pathname}/page.mdx`}
+					target="_blank"
+					prefetch={false}
+					className="mr-auto flex text-sm items-center gap-2 text-slate-500 dark:text-slate-400 rounded-sm focus-ring hover:underline"
+				>
+					<IconGitHub width={20} height={20} />
+					Edit this page on GitHub
+				</Link>
 			</div>
 		</footer>
 	)
